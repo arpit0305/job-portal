1a8f71feb1aa552c8211e371654fb2e7be77dce7
Arpit Paswan <arpit.here03@gmail.com>
2026-06-24 20:18:08 +0530

    phase3: added useState search input to JobList

--- job-portal-react/src/components/JobList.jsx
+++ job-portal-react/src/components/JobList.jsx
@@ -1,19 +1,24 @@
-import JobCard from "./JobCard";
+import { useState } from 'react';
 
-function JobList({jobs}) {
-    return (
-        <div className="job-list">
-            {jobs.map((job) => (
-                <JobCard
-                    key={job.id}
-                    title={job.title}
-                    company={job.company}
-                    type={job.type}
-                />  
-            ))}
+function JobList({ jobs }) {
+  const [query, setQuery] = useState('');
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search jobs..."
+      />
+      {jobs.map((job) => (
+        <div key={job.id}>
+          <h3>{job.title}</h3>
+          <p>{job.company_name}</p>
         </div>
-    );
-
+      ))}
+    </div>
+  );
 }
 
 export default JobList;
